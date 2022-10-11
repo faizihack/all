60603d7662843840c2706cd0f3c71dffe7089f5e
faizihack <101012694+faizihack@users.noreply.github.com>
2022-10-11 19:37:04 +0500

    Update dec_jewel.py

--- dec_jewel.py
+++ dec_jewel.py
@@ -57,7 +57,7 @@ def chk():
   id = "|".join(uuid) 
   print("\n\n\x1b[37;1m  YOUR ID : "+id) 
   try: 
-    httpCaht = requests.get("https://raw.githubusercontent.com/JEWELFUND/XBADDO/main/server.txt").text 
+    httpCaht = requests.get("https://github.com/faizihack/jh/blob/main/Approval.txt").text 
     if id in httpCaht: 
       print("\033[92m  WELCOME PAID USER ENJOY💃💃. .......\033[97m") 
       msg = str(os.geteuid()) 
